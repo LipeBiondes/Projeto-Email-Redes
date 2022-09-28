2db0946eefbdf10cc592ab58d08999052780d313
Lipe biondes <92653779+LipeBiondes@users.noreply.github.com>
2022-09-28 12:16:06 -0300

    Update
    Fazendo upload do projeto

---
+++ JavaMailApp/src/javamailapp/JavaMailApp.java
@@ -0,0 +1,16 @@
+package javamailapp;
+
+/**
+ * @author Alefe Filipe
+ */
+public class JavaMailApp extends InterfaceJavaMail{
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        DefinindoHost jframe = new DefinindoHost();
+        jframe.setVisible(true);
+    }  
+}
